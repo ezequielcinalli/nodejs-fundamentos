1d2c3a0b4f43adab40dc8f54f976176fcdfc4be1
ezequiel <ezequielcinalli96@gmail.com>
2020-12-16 19:01:00 -0300

    Add CRUD pets with mongoDB

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+    // parse application/json
+app.use(bodyParser.json())
+
 require('dotenv').config();
+
 const port = process.env.PORT || 3000;
 
 //Conexion mongodb
--- router/pets.js
+++ router/pets.js
@@ -13,4 +13,57 @@ router.get('/', async(req, res) => {
 
 });
 
+router.get('/create', (req, res) => {
+    res.render("pets/create");
+});
+
+router.post('/', async(req, res) => {
+    const body = req.body;
+    try {
+        const petDB = new Pet(body);
+        await petDB.save();
+        res.redirect('/pets');
+    } catch (e) {
+        console.log("Error: " + e);
+    }
+});
+
+router.get('/:id', async(req, res) => {
+    try {
+        const id = req.params.id;
+        const petDB = await Pet.findOne({ _id: id });
+        res.render("pets/detail", { pet: petDB, error: false });
+    } catch (e) {
+        res.render("pets/detail", { error: true });
+
+    }
+});
+
+router.delete('/:id', async(req, res) => {
+    const id = req.params.id;
+    try {
+        const petDB = await Pet.findByIdAndDelete({ _id: id });
+        if (petDB) {
+            res.json({ state: true, })
+        } else {
+            res.json({ state: false, })
+        }
+    } catch (e) {
+        console.log(e);
+    }
+});
+
+router.put('/:id', async(req, res) => {
+    const id = req.params.id;
+    const body = req.body;
+    try {
+        const petDB = await Pet.findByIdAndUpdate(id, body, { useFindAndModify: false });
+        console.log(petDB);
+        res.json({ state: true, })
+    } catch (e) {
+        console.log("Error: " + e);
+        res.json({ state: false, })
+    }
+});
+
 module.exports = router
